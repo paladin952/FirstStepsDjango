9e7fce264872c418ca5fa0c698cbe07497009c90
Clapa Lucian <clapa_lucian@yahoo.com>
2016-02-25 23:42:58 +0200

    added different highlight for current tutorial in side bar

--- videos/models.py
+++ videos/models.py
@@ -1,44 +1,45 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Tutorial(models.Model):
-	name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
-	def __str__(self):
-		return str(self.name)
+    def __str__(self):
+        return str(self.name)
 
 
 class Video(models.Model):
-	title = models.CharField(max_length=200)
-	description = models.TextField(max_length=5000)
-	upload_date = models.DateTimeField(default=timezone.now)
-	url = models.CharField(max_length=500, default="/")
-	tags = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=5000)
+    upload_date = models.DateTimeField(default=timezone.now)
+    url = models.CharField(max_length=500, default="/")
+    tags = models.CharField(max_length=200)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	def __str__(self):
-		return str(self.title)
+    def __str__(self):
+        return str(self.title)
 
-	def get_absolute_url(self):
-		return '/video/%s/' % self.id
+    def get_absolute_url(self):
+        return '/video/%s/' % self.id
 
 
 class TutorialVideos(models.Model):
-	tutorial = models.ForeignKey(Tutorial)
-	video = models.ForeignKey(Video)
+    tutorial = models.ForeignKey(Tutorial)
+    video = models.ForeignKey(Video)
 
-	# def get_absolute_url(self):
-	# 	return '/%s/%s/' % (self.tutorial.name)
+    # def get_absolute_url(self):
+    # 	return '/%s/%s/' % (self.tutorial.name)
 
-	def __unicode__(self):
-		return self.tutorial + ": " + self.video
+    def __unicode__(self):
+        return self.tutorial + ": " + self.video
 
-	def __str__(self):
-		return str(self.tutorial) + ": " + str(self.video)
+    def __str__(self):
+        return str(self.tutorial) + ": " + str(self.video)
--- videos/views.py
+++ videos/views.py
@@ -1,64 +1,65 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template.response import TemplateResponse
-from .models import Tutorial
-from .models import Video
-from .models import TutorialVideos
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+from .models import Tutorial
+from .models import TutorialVideos
+from .models import Video
+
+
 # Create your views here.
 
 
 def video(request):
-	title = "VIDEOS"
-	sidebar_title = "All tutorials"
+    title = "VIDEOS"
+    sidebar_title = "All tutorials"
 
-	tutorials_list = Tutorial.objects.all()
-	videos_list = Video.objects.all()
-	tutorial_videos_list = TutorialVideos.objects.all()
+    tutorials_list = Tutorial.objects.all()
+    videos_list = Video.objects.all()
+    tutorial_videos_list = TutorialVideos.objects.all()
 
-	context = {
-		"title": title,
-		"tutorials_list": tutorials_list,
-		"videos_list": videos_list,
-		"tutorial_videos_list": tutorial_videos_list,
-		"sidebar_title": sidebar_title,
-		}
-	return render(request, "videos/index.html", context)
+    context = {
+        "title": title,
+        "tutorials_list": tutorials_list,
+        "videos_list": videos_list,
+        "tutorial_videos_list": tutorial_videos_list,
+        "sidebar_title": sidebar_title,
+    }
+    return render(request, "videos/index.html", context)
 
 
 def tutorial_videos(request, tutorial="android"):
-	title = "VIDEOS"
-	sidebar_title = "All tutorials"
+    title = "VIDEOS"
+    sidebar_title = "All tutorials"
 
-	tutorials_list = Tutorial.objects.all()
-	videos_list = Video.objects.all()
-	tutorial_videos_list = TutorialVideos.objects.all()
-	
-	context = {
-		"title": title,
-		"tutorials_list": tutorials_list,
-		"videos_list": videos_list,
-		"tutorial_videos_list": tutorial_videos_list,
-		"sidebar_title": sidebar_title,
-		"current_tutorial": tutorial.lower(),
-		}
-	return render(request, "videos/tutorial_videos.html", context)
+    tutorials_list = Tutorial.objects.all()
+    videos_list = Video.objects.all()
+    tutorial_videos_list = TutorialVideos.objects.all()
+
+    context = {
+        "title": title,
+        "tutorials_list": tutorials_list,
+        "videos_list": videos_list,
+        "tutorial_videos_list": tutorial_videos_list,
+        "sidebar_title": sidebar_title,
+        "current_tutorial": tutorial.lower(),
+    }
+    return render(request, "videos/tutorial_videos.html", context)
 
 
 def video_detail(request, pk=1):
-	title = "VIDEOS"
-	sidebar_title = "All tutorials"
+    title = "VIDEOS"
+    sidebar_title = "All tutorials"
 
-	tutorials_list = Tutorial.objects.all()
-	videos_list = Video.objects.all()
-	tutorial_videos_list = TutorialVideos.objects.all()
-	video = get_object_or_404(Video, pk=pk)
-	context = {
-		"title": title,
-		"tutorials_list": tutorials_list,
-		"videos_list": videos_list,
-		"tutorial_videos_list": tutorial_videos_list,
-		"sidebar_title": sidebar_title,
-		"video": video,
-		}
-	return render(request, "videos/video_post.html", context)
+    tutorials_list = Tutorial.objects.all()
+    videos_list = Video.objects.all()
+    tutorial_videos_list = TutorialVideos.objects.all()
+    video = get_object_or_404(Video, pk=pk)
+    context = {
+        "title": title,
+        "tutorials_list": tutorials_list,
+        "videos_list": videos_list,
+        "tutorial_videos_list": tutorial_videos_list,
+        "sidebar_title": sidebar_title,
+        "video": video,
+    }
+    return render(request, "videos/video_post.html", context)
